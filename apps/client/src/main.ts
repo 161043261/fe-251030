83f0de3853d9fcaa18ad808d1f65d81d16cf8d9e
larkhang <larkhang@tencent.com>
2025-12-11 20:42:59 +0800

    feat: Introduce new features

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,7 +1,36 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
+import i18next from "i18next";
+import I18NextVue from "i18next-vue";
+import enCommon from './locales/en/common.json'
+import zhCommon from './locales/zh/common.json'
+
+const resources = {
+  en: { messages: enCommon },
+  zh: { messages: zhCommon },
+}
+
 export function createApp() {
+
+  i18next.init({
+    resources,
+    lng: "en",
+    fallbackLng: "en",
+    keySeparator: '.',
+    interpolation: {
+      escapeValue: false,
+    },
+    ns: ['common'],
+    defaultNS: 'common',
+    fallbackNS: 'common',
+    backend: {
+      loadPath: './locales/{{lng}}/{{ns}}.json',
+    },
+  });
+
   const app = createSSRApp(App);
+  app.use(I18NextVue, { i18next })
+    // .mount('#app');
   return {
     app,
   };
--- apps/client/src/pages/auth/components/login.vue
+++ apps/client/src/pages/auth/components/login.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
+import { useTranslation } from 'i18next-vue';
 
+const { i18next, t } = useTranslation();
+const formData = reactive({
+  username: '',
+  password: '',
+})
 </script>
 
 <template>
-  <view>登录</view>
+  <view>
+    <h1>{{ i18next.language }}</h1>
+    <uni-form>
+      <uni-forms-item :label="t('账号')" name="username" prop="username" required>
+        <uni-easyinput v-model="formData.username" type="text" :placeholder="t('请输入账号')" />
+      </uni-forms-item>
+      <uni-forms-item required :label="$t('密码')" name="password" prop="password">
+        <uni-easyinput v-model="formData.password" type="password" :placeholder="$t('请输入密码')" />
+      </uni-forms-item>
+    </uni-form>
+  </view>
 </template>
 
-<style scoped lang="css">
-
-</style>
+<style scoped lang="css"></style>
